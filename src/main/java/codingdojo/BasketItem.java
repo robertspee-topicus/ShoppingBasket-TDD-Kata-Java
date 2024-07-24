eaece03d2bbe8546d2706970cc3437c7b319af12
robert.spee <robert.spee@topicus.nl>
2024-07-24 16:05:12 +0200

    Initial commit

---
+++ src/main/java/codingdojo/BasketItem.java
@@ -0,0 +1,9 @@
+package codingdojo;
+
+import java.math.BigDecimal;
+
+public class BasketItem {
+    public BasketItem(String name, BigDecimal price) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+}
---
+++ src/main/java/codingdojo/ShoppingBasket.java
@@ -0,0 +1,17 @@
+package codingdojo;
+
+import java.math.BigDecimal;
+
+public class ShoppingBasket {
+    public void add(BasketItem item, double quantity) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    public int getQuantity(String itemName) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    public BigDecimal calculateTotal() {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+}
